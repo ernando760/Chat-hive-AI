8bc983c7fb236fb3cf0c0f24de459a2a3cfe01fc
ErnandoM <ernandomanoel.freire@gmail.com>
2024-04-09 09:52:54 -0300

    refactor: making some small alters

--- lib/src/shared/routes/app_router.dart
+++ lib/src/shared/routes/app_router.dart
@@ -1,12 +1,14 @@
+import 'package:chat_hive_ai/src/modules/auth/presenter/screens/splash/splash_page.dart';
 import 'package:chat_hive_ai/src/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
   AppRouter._();
 
-  static const String initialRoute = "/";
+  static const String initialRoute = "/splash";
 
   static final Map<String, Widget Function(BuildContext context)> routes = {
-    AppRouter.initialRoute: (context) => const HomePage(),
+    AppRouter.initialRoute: (context) => const SplashPage(),
+    "/home": (context) => const HomePage()
   };
 }
