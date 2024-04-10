e5c9848a7b0e667e389bad8a39d98b4a08226881
ErnandoM <ernandomanoel.freire@gmail.com>
2024-04-10 11:23:14 -0300

    test: criando testes dos casos de uso [create_chat_usecase_test.dart,find_all_chats_of_user_usecase_test.dart,delete_chat_usecase_test.dart,update_chat_usecase_test.dart]

---
+++ test/src/modules/home/domain/usecases/update_chat_usecase_test.dart
@@ -0,0 +1,85 @@
+import 'package:chat_hive_ai/src/modules/home/domain/entities/chat_entity.dart';
+import 'package:chat_hive_ai/src/modules/home/domain/repositories/update_chat_repository.dart';
+import 'package:chat_hive_ai/src/modules/home/domain/usecases/update_chat_usecase.dart';
+import 'package:chat_hive_ai_core/chat_hive_ai_core.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+
+import '../../../../../mock/user_mock.dart';
+
+class MockUpdateChatRepository extends Mock implements UpdateChatRepository {}
+
+void main() {
+  final UpdateChatRepository repository = MockUpdateChatRepository();
+  final UpdateChatUsecase usecase = UpdateChatUsecase(repository);
+
+  List<ChatEntity> updateChat(String id, String title) {
+    return chats.map(
+      (element) {
+        if (element.chatId != id) {
+          return element;
+        }
+        element = ChatEntity(chatId: id, title: title, messages: messages);
+        return element;
+      },
+    ).toList();
+  }
+
+  test(
+      "Deve retornar um lista de conversas quando chamo a função [findAllchatsOfUser]",
+      () async {
+    final newChats = updateChat("chat5", "chat update");
+    when(
+      () => repository
+          .call(ids: (userId: "userId", chatId: "chat5"), title: "chat update"),
+    ).thenAnswer((_) async => Success(newChats));
+
+    final res = await usecase(
+        ids: (userId: "userId", chatId: "chat5"), title: "chat update");
+    expect(res.success, newChats);
+    expect(
+        res.success
+            .firstWhere((element) => element.title == "chat update")
+            .title,
+        equals(newChats
+            .firstWhere((element) => element.title == "chat update")
+            .title));
+    expect(res.success, isA<List<ChatEntity>>());
+  });
+
+  test(
+      "Deve retornar uma Exceção com a messagem dizendo 'O id do usuário está vazio.', quando chamo a função [updateChatUsecase.call]",
+      () async {
+    final res =
+        await usecase(ids: (userId: "", chatId: "chat5"), title: "chat update");
+
+    expect(res.failure.messageErro, 'O id do usuário está vazio.');
+  });
+
+  test(
+      "Deve retonar uma exceção com uma menssagem dizendo 'O id da conversa está vazio.' quando chamo a função [updateChatUsecase.call]",
+      () async {
+    final res = await usecase(
+        ids: (userId: "userId", chatId: ""), title: "chat update");
+
+    expect(res.failure.messageErro, "O id da conversa está vazio.");
+  });
+
+  test(
+      "Deve retonar uma exceção com uma menssagem dizendo 'O titulo não pode está vazio.' quando chamo a função [updateChatUsecase.call]",
+      () async {
+    final res =
+        await usecase(ids: (userId: "userId", chatId: "chat5"), title: "");
+
+    expect(res.failure.messageErro, "O titulo não pode está vazio.");
+  });
+  test(
+      "Deve retonar uma exceção com uma menssagem dizendo 'O id do usuário e da conversa está vazio.' quando chamo a função [updateChatUsecase.call]",
+      () async {
+    final res =
+        await usecase(ids: (userId: null, chatId: null), title: "chat update");
+
+    expect(
+        res.failure.messageErro, "O id do usuário e da conversa está vazio.");
+  });
+}
