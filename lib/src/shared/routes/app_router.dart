fee91cc45fe348194b89e73b0201629325d5b755
ErnandoM <ernandomanoel.freire@gmail.com>
2024-04-23 16:17:03 -0300

    fix: corrigindo as rotas

--- lib/src/shared/routes/app_router.dart
+++ lib/src/shared/routes/app_router.dart
@@ -1,5 +1,7 @@
+import 'package:chat_hive_ai/src/modules/auth/domain/entities/user_model.dart';
 import 'package:chat_hive_ai/src/modules/auth/presenter/screens/splash/splash_page.dart';
-import 'package:chat_hive_ai/src/screens/home_page.dart';
+import 'package:chat_hive_ai/src/modules/home/presenter/screens/home_page.dart';
+import 'package:chat_hive_ai/src/modules/home/presenter/widgets/wrapper/home_provider_wrapper_widget.dart';
 import 'package:flutter/material.dart';
 
 class AppRouter {
@@ -8,7 +10,10 @@ class AppRouter {
   static const String initialRoute = "/splash";
 
   static final Map<String, Widget Function(BuildContext context)> routes = {
-    AppRouter.initialRoute: (context) => const SplashPage(),
-    "/home": (context) => const HomePage()
+    "/splash": (context) => const SplashPage(),
+    "/home": (context) {
+      final user = ModalRoute.of(context)?.settings.arguments as UserEntity?;
+      return HomeProviderWrapperWidget(child: HomePage(user: user));
+    }
   };
 }
